da893cc0b6a15d8a96f7c3c63f47c5d4eb4e7c4f
Hirou <82706798+thehirou@users.noreply.github.com>
2023-10-24 22:43:15 +0200

    updatttttttttt

---
+++ projects/webcomic/scripts/nav-consts.js
@@ -0,0 +1,5 @@
+const rightNav = document.getElementById('rightNav');
+rightNav.href = destinationUrls.right;
+
+const leftNav = document.getElementById('leftNav');
+leftNav.href = destinationUrls.left;
