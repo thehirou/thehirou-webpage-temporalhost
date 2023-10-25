116f99f3c47bd0676a7b25c17b3d5f8c4ed130e8
Hirou <82706798+thehirou@users.noreply.github.com>
2023-10-25 14:39:28 +0200

    improved scripts

--- projects/webcomic/scripts/toggle-blather.js
+++ projects/webcomic/scripts/toggle-blather.js
@@ -2,10 +2,9 @@
 const outerBlatherbox = document.getElementById('outer-blatherbox');
 const blatherbox = document.getElementById('blatherbox');
 const toggleButton = document.getElementById('toggleButton');
-let isToggled = false;
 
-toggleButton.addEventListener('click', () => {
-    isToggled = !isToggled;
+// Add this function to set the initial styles based on isToggled
+function setInitialStyles() {
     const newWidth = isToggled ? '0px' : '640px';
     outerBlatherbox.style.setProperty('--blatherbox-width', newWidth);
 
@@ -17,11 +16,45 @@ toggleButton.addEventListener('click', () => {
     // Update the --transition-speed variable
     document.documentElement.style.setProperty('--transition-speed', transitionDuration);
 
+    updateButtonText(); // Update the button text based on canToggle
+
     updateMainCenteredWidth();
 
     const borderWidth = isToggled ? '0px' : '4px';
     blatherbox.style.borderWidth = borderWidth;
+}
+
+// Call the function to set the initial styles when the page loads
+setInitialStyles();
+
+toggleButton.addEventListener('click', () => {
+    if (canToggle) {
+        isToggled = !isToggled;
+        setInitialStyles(); // Set the styles based on the new state
+    }
 });
+
+// Function to enable or disable the toggle button
+function toggleButtonState(enable) {
+    canToggle = enable;
+    toggleButton.disabled = !enable;
+    updateButtonText(); // Update the button text based on canToggle
+}
+
+// Function to update the button text based on canToggle
+function updateButtonText() {
+    if (canToggle) {
+        toggleButton.textContent = "Toggle Blatherbox";
+    } else {
+        toggleButton.textContent = "...";
+    }
+}
+
+// Example: To disable the button
+// toggleButtonState(false);
+
+// Example: To enable the button
+// toggleButtonState(true);
 
 function updateMainCenteredWidth() {
     const imageWidth = window.getComputedStyle(outerBlatherbox).getPropertyValue('--image-width');
