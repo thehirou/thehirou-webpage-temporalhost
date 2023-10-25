7fe5caee610bfc9a13b4272bb02ebae5da7e08d9
Hirou <82706798+thehirou@users.noreply.github.com>
2023-10-25 02:29:14 +0200

    updates everywhhhrhrrrrrwwww

--- projects/webcomic/scripts/toggle-blather.js
+++ projects/webcomic/scripts/toggle-blather.js
@@ -1,29 +1,4 @@
 // GOTTA STUDY THIS CODE LATER BECAUSE I HAVE NO IDEA HOW IT REALLY WORKS
-/* 
-const outerBlatherbox = document.getElementById('outer-blatherbox');
-const blatherbox = document.getElementById('blatherbox');
-const toggleButton = document.getElementById('toggleButton');
-let isToggled = false;
-
-toggleButton.addEventListener('click', () => {
-    isToggled = !isToggled;
-    const newWidth = isToggled ? '0px' : '640px';
-    outerBlatherbox.style.setProperty('--blatherbox-width', newWidth);
-
-    updateMainCenteredWidth();
-
-    const borderWidth = isToggled ? '0px' : '4px';
-    blatherbox.style.borderWidth = borderWidth;
-});
-
-function updateMainCenteredWidth() {
-    const imageWidth = window.getComputedStyle(outerBlatherbox).getPropertyValue('--image-width');
-    const blatherboxWidth = window.getComputedStyle(outerBlatherbox).getPropertyValue('--blatherbox-width');
-    const mainCenteredWidth = `calc(${imageWidth} + ${blatherboxWidth})`;
-    document.documentElement.style.setProperty('--main-centered-width', mainCenteredWidth);
-}
-*/
-
 const outerBlatherbox = document.getElementById('outer-blatherbox');
 const blatherbox = document.getElementById('blatherbox');
 const toggleButton = document.getElementById('toggleButton');
